fbebbacebe0bf14f47c4427303bc0dd6e4bc039d
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2025-06-19 10:02:14 -0500

    feat: migrate from Create React App to Vite

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="element-tray">
+        <div className="element-placeholder" />
+        <div className="element-placeholder" />
+        <div className="element-placeholder" />
+        <div className="element-placeholder" />
+        <div className="element-placeholder" />
+        <div className="element-placeholder" />
+      </div>
+      <div className="form-canvas" />
+    </>
+  );
+}
+
+export default App;
